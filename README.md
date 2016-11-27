24116a24910b84290edd33d6d31245577c9e373e
Daniel Griffiths <logiconictvtube@gmail.com>
2016-11-26 20:18:50 -0500

    cleaned up variable names, started readme, ready for revamp

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-This game of pick up sticks involves a programmed computer player, player two.
-Will include the finnished edit of optional p v p, or p v ai.
-Learning how to utilize classes for orginazational purposes.
+This was one of the earlier homework assignments for The Iron Yard.
+
+This game of pick up sticks is built with a very simplistic OOP class
+structure. It allows for a game to be played between a player and
+computer with a simple randomized algorithm.
--- pick_up_sticks.rb
+++ pick_up_sticks.rb
@@ -1,29 +1,29 @@
 class Sticks
-  attr_accessor :stick_amount, :p_1_s_t, :p_2_s_t, :a_i
+  attr_accessor :stick_amount, :player_1_sticks_taken, :player_2_sticks_taken, :computer
 
-  def initialize(stick_amount, p_1_s_t, p_2_s_t, a_i)
+  def initialize(stick_amount, player_1_sticks_taken, player_2_sticks_taken, computer)
     @stick_amount = stick_amount
-    @p_1_s_t = p_1_s_t
-    @p_2_s_t = p_2_s_t
-    @a_i = a_i
+    @player_1_sticks_taken = player_1_sticks_taken
+    @player_2_sticks_taken = player_2_sticks_taken
+    @computer = computer
   end
 
-  def get_ai_pickup_sticks
+  def get_computer_pickup_sticks
     if @stick_amount > 5
-      @a_i = rand(1..5)
+      @computer = rand(1..5)
     else
-      @a_i = 5
+      @computer = 5
     end
   end
 
-  def p_1_stick_change(p_1)
-    @p_1_s_t += p_1
-    @stick_amount -= p_1
+  def player_1_stick_change(player_1)
+    @player_1_sticks_taken += player_1
+    @stick_amount -= player_1
   end
 
-  def p_2_stick_change(p_2)
-    @p_2_s_t += p_2
-    @stick_amount -= p_2
+  def player_2_stick_change(player_2)
+    @player_2_sticks_taken += player_2
+    @stick_amount -= player_2
   end
 end
 
@@ -36,23 +36,23 @@ game = Sticks.new(50, 0, 0, rand(1..5))
 until game.stick_amount <= 0.to_i do
   puts "There are #{game.stick_amount} sticks on the table."
   puts "Player One, pick up 1-5 sticks"
-    p_1 = gets.chomp.to_i
-  if p_1 > 5
+    player_1 = gets.chomp.to_i
+  if player_1 > 5
     puts "Player One, pick up 1-5 sticks or you will forfiet your turn and one stick will be added."
-      p_1 = gets.chomp.to_i
+      player_1 = gets.chomp.to_i
       game.stick_amount += 1
-  elsif p_1 < 1
+  elsif player_1 < 1
     puts "Player One, pick up 1-5 sticks or your turn will be forfiet and one stick will be added."
-      p_1 = gets.chomp.to_i
+      player_1 = gets.chomp.to_i
       game.stick_amount += 1
   else
-      game.p_1_stick_change(p_1)
+      game.player_1_stick_change(player_1)
   end
   puts "There are #{game.stick_amount} sticks on the table"
   puts "Player Two, pick up 1-5 sticks"
-  game.get_ai_pickup_sticks
-  p_2 = game.a_i
-  puts p_2
-  game.p_2_stick_change(p_2)
+  game.get_computer_pickup_sticks
+  player_2 = game.computer
+  puts player_2
+  game.player_2_stick_change(player_2)
 end
 puts "GAME OVER MOTHAFUCKA"
