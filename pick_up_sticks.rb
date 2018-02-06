bc0fc20c0507e180ec65eb7e3871e707066b67e9
Daniel Griffiths <Logiconictvtube@gmail.com>
2018-02-05 22:26:45 -0500

    Update pick_up_sticks.rb

--- pick_up_sticks.rb
+++ pick_up_sticks.rb
@@ -27,16 +27,14 @@ class Sticks
   end
 end
 
-
 game = Sticks.new(50, 0, 0, rand(1..5))
 
-
-
-
 until game.stick_amount <= 0.to_i do
+  
   puts "There are #{game.stick_amount} sticks on the table."
   puts "Player One, pick up 1-5 sticks"
     player_1 = gets.chomp.to_i
+  
   if player_1 > 5
     puts "Player One, pick up 1-5 sticks or you will forfiet your turn and one stick will be added."
       player_1 = gets.chomp.to_i
@@ -48,11 +46,14 @@ until game.stick_amount <= 0.to_i do
   else
       game.player_1_stick_change(player_1)
   end
+  
   puts "There are #{game.stick_amount} sticks on the table"
   puts "Player Two, pick up 1-5 sticks"
   game.get_computer_pickup_sticks
   player_2 = game.computer
   puts player_2
   game.player_2_stick_change(player_2)
+  
 end
-puts "GAME OVER MOTHAFUCKA"
+
+puts "GAME OVER"
